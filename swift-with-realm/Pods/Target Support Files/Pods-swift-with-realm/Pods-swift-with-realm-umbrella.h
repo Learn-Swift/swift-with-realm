9e78bd6a02eea778f48d2eb778a4b27beb2bae48
Duc Nguyen <nnduc@smartdev.vn>
2015-12-28 22:04:27 +0700

    initial

---
+++ swift-with-realm/Pods/Target Support Files/Pods-swift-with-realm/Pods-swift-with-realm-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_swift_with_realmVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_swift_with_realmVersionString[];
+
---
+++ swift-with-realm/swift-with-realmTests/swift_with_realmTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_with_realmTests.swift
+//  swift-with-realmTests
+//
+//  Created by Duc Nguyen on 12/28/15.
+//  Copyright © 2015 ND NINJA. All rights reserved.
+//
+
+import XCTest
+@testable import swift_with_realm
+
+class swift_with_realmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
